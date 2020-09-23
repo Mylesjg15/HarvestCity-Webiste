7133c8708c7602b31bd5e14bc6c0895b8115173e
Mylesjg15 <mylesjg15@gmail.com>
2020-09-23 11:06:39 -0500

    More structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { GiveComponent } from './components/give/give.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    GiveComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
